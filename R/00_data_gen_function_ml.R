52f6502bdf8b06d3d10197d9cdf1d8e04fbfacbb
lilykoff <koffmanlily@gmail.com>
2025-02-13 15:32:58 -0500

    add multilevel sim

--- R/00_data_gen_function_ml.R
+++ R/00_data_gen_function_ml.R
@@ -18,60 +18,29 @@ library(lme4)
 library(mgcv)
 library(ggplot2)
 library(gridExtra)
-
+library(mvtnorm)
+library(splines)
 invisible(lapply(required_packages, install_and_load))
 
 
-## specify true fixed effects functions
-# grid <- seq(0, 1, length = L)
-# beta_true <- matrix(NA, 2, L) # time-dependent beta-true trajectory
-# scenario <- 1 ## indicate the scenario to use
-# if(scenario == 1){
-#   beta_true[1,] = -0.15 - 0.1*sin(2*grid*pi) - 0.1*cos(2*grid*pi)
-#   beta_true[2,] = dnorm(grid, .6, .15)/20
-# }else if(scenario == 2){
-#   beta_true[1,] <- 0.53 + 0.06*sin(3*grid*pi) - 0.03*cos(6.5*grid*pi)
-#   beta_true[2,] <- dnorm(grid, 0.2, .1)/60 + dnorm(grid, 0.35, .1)/200 -
-#     dnorm(grid, 0.65, .06)/250 + dnorm(grid, 1, .07)/60
-# }
-# rownames(beta_true) <- c("Intercept", "x")
-#
-# I = 10e6 # superpopulation size
-# set.seed(4574)
-# X_des = cbind(1, rnorm(I, 0, 2)) # design matrix: Intercept + predicted variables
-# true_fixef <- X_des %*% beta_true
-# L = 50
 
-
-
-# Y_obs <- matrix(NA, nrow = I, ncol = L)
-# for(i in 1:nrow(true_fixef)){
-#   for(j in 1:L){
-#     Y_obs[i, j] <- rnorm(1, mean = true_fixef[i, j], sd = SNR_sigma)
-#   }
-# }
-
-
-# set.seed(4574)
-# Y_obs <- matrix(
-#   rnorm(n = nrow(true_fixef) * L, mean = as.vector(t(true_fixef)), sd = SNR_sigma),
-#   nrow = nrow(true_fixef),
-#   ncol = L,
-#   byrow = TRUE
-# )
-#
-
-
-simulate_survey_fosr <- function(X_des, # design matrix
-                                 beta_true, # true beta
-                                 true_fixef, # true fixed effects (Y)
-                                 Y_obs, # y matrix
+simulate_survey_fosr_ml <- function(X_des, # design matrix
+                                 X_df, # design matrix with ID and subject indices
+                                 eta_true, # true fixed effects with random eff
+                                 Y_obs,
+                                 J_subj, # num visits per person
                                  I_n = 100, # subjects in each psu-strata combination
                                  alpha = 2, # number PSU
                                  L = 50, # dimension of the functional domain
                                  num_strata = 30,  # total strata
                                  sampled_strata = 30
 ){
+  # do the selection on the ID level
+  # summarize X variable by ID
+  X_summ =
+    X_df %>%
+    group_by(id) %>%
+    summarize(X2 = mean(V2))
 
   # Generate strata sizes using a Dirichlet distribution
   dirichlet_probs = gtools::rdirichlet(1, rep(1, num_strata)) # vector that sums to 1, of length num_strata
@@ -95,7 +64,7 @@ simulate_survey_fosr <- function(X_des, # design matrix
 
   for (strata in selected_strata) {
     inds <- which(stratum_assignments == strata)
-    inclusion_probs <- exp(X_des[inds, 2])  # Compute individual inclusion probabilities based on covariate
+    inclusion_probs <- exp(X_summ$X2[inds])
     inclusion_probs <- inclusion_probs / sum(inclusion_probs) # normalize
     inclusion_probs <- inclusion_probs * I_n * 2 # Scale to get the desired number of individuals in each strata / PSU combo
     # summary(inclusion_probs) # print inclusion probabilities
@@ -106,31 +75,47 @@ simulate_survey_fosr <- function(X_des, # design matrix
     psu_strata <- rbinom(length(sampled_in_strata), 1, 0.5) + 1 # randomly assign PSUs in strata
     psu <- c(psu, psu_strata)
   }
-  # length(final_sample) # 2939, close to 30 * 50 * 2
 
   # Compute final individual selection probabilities
   p_i <- p_strata[stratum_assignments] * p_i_given_strata
   survey_weights <- 1 / p_i  # Final survey weights (Inf: Not selected)
 
-  dat.sim <- data.frame(
-    ID = final_sample,
-    X = X_des[final_sample,2],
-    strata = stratum_assignments[final_sample], # Assume same strata here ??
-    psu = psu,
-    weight = survey_weights[final_sample]
-  )
+  # get the indices associated with the final sample ids
+  indices =
+    X_df %>%
+    filter(id %in% final_sample) %>%
+    pull(index)
 
-  Y_sample <- data.frame(Y_obs[final_sample,])
-  colnames(Y_sample) <- paste0("Y", 1:L)
 
-  sample_data <- cbind(dat.sim, Y_sample)
-  sample_data <- sample_data %>%
+  X_des_sample = X_des[indices,]
+  eta_sample = eta_true[indices,]
+
+  visits = X_df %>%
+    filter(id %in% final_sample) %>%
+    pull(visit)
+
+  ids_long = X_df %>%
+    filter(id %in% final_sample) %>%
+    pull(id)
+
+  Y_sample = Y_obs[indices,]
+
+  strata = rep(stratum_assignments[final_sample], J_subj[final_sample]) # repeat strata assignments based on visit nums
+  weights = rep(survey_weights[final_sample], J_subj[final_sample]) # repeat weights based on visit nums
+  psu = rep(psu, J_subj[final_sample])
+  ## here
+  dat.sim <- data.frame(ID = ids_long,
+                        visit = visits,
+                        X = X_des_sample[,2],
+                        strata = strata,
+                        weight = weights,
+                        psu = psu,
+                        eta = I(eta_sample),
+                        Y = I(Y_sample))
+
+
+  sample_data <- dat.sim %>%
     filter(!is.na(weight)) # shouldn't happen but just in case
 
   return(sample_data)
 }
-# dat = replicate(100, simulate_survey_fosr(), simplify = FALSE)
-# write_rds(dat, here::here("sim_data", "dat_f1.rds"), compress = "xz")
-
-# data = simulate_survey_fosr()
-# write_rds(dat, here::here("sim_data", "dat_f2.rds"), compress = "xz")
