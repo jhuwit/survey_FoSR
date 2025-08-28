0c881c860e75971fd99ade96f06cd9fc7d278354
lilykoff <koffmanlily@gmail.com>
2025-08-28 12:33:10 -0400

    fix inf level

--- R/03_run_simulation_infmax2.R
+++ R/03_run_simulation_infmax2.R
@@ -90,7 +90,7 @@ if(!file.exists(outfile) || force) {
         psu_assignments = lst$psu_assignments,
         dirichlet_probs = lst$dirichlet_probs,
         seed = iter,
-        inf_level = temp$inf_level * 2,
+        inf_level = temp$inf_level * 3,
         compression = 2,
       )
       print(summary(data$weight))
