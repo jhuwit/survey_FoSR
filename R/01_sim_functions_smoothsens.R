2f7283cedb0e5155c486fb47d68d925735e9c45c
lilykoff <koffmanlily@gmail.com>
2026-08-24 15:31:58 -0400

    add smoothing sensitivity code

---
+++ R/01_sim_functions_smoothsens.R
@@ -0,0 +1,180 @@
+library(dplyr)
+settings_small =
+  settings |>
+  dplyr::filter(
+    (family == "gaussian" & strata_scale == .125 & strata_sigma == 0.05 &
+       snr_b == 0.5 & snr_eps == 1 & inf_level == 10 & In == 100) |
+      (family %in% c("binomial", "poisson") & strata_scale == .125 &
+         strata_sigma == 0.05 & snr_b == 0.5 & inf_level == 10 & In == 100)
+  )
+
+
+sensitivity_grid = tidyr::expand_grid(
+  nknots_spec = c("conservative", "current", "flexible"),
+  splines = c("tp", "cr", "ps"),
+  method = c("GCV.Cp", "REML")
+)
+
+
+full_grid = tidyr::expand_grid(
+  nknots_spec = c("conservative", "current", "flexible"),
+  splines = c("tp", "cr", "ps"),
+  method = c("GCV.Cp", "REML"),
+  family = c("gaussian", "binomial", "poisson"),
+  strata_scale = 0.125,
+  strata_sigma = 0.05,
+  inf_level = 10,
+  In = 100,
+  snr_b = 0.5,
+  snr_eps = 1,
+  len = c(50, 100, 1440))
+
+full_grid =
+  full_grid |>
+  mutate(snr_eps = if_else(family == "gaussian", snr_eps, NA_real_))
+
+
+smaller_grid =
+  full_grid |>
+  filter((splines == "tp" & method == "GCV.Cp") |
+           (nknots_spec == "current" & splines == "cr" & method == "GCV.Cp") |
+           (nknots_spec == "current" & splines == "tp" & method == "REML") |
+           (nknots_spec == "current" & splines == "ps" & method == "REML")
+  ) |>
+  mutate(fold = row_number())
+# key_scenarios <- list(
+#   baseline = list(nknots = "current", splines = "tp", method = "GCV.Cp"),
+#   fewer_knots = list(nknots = "conservative", splines = "tp", method = "GCV.Cp"),
+#   more_knots = list(nknots = "flexible", splines = "tp", method = "GCV.Cp"),
+#   alt_basis = list(nknots = "current", splines = "cr", method = "GCV.Cp"),
+#   alt_method = list(nknots = "current", splines = "tp", method = "REML"),
+#   penalized = list(nknots = "current", splines = "ps", method = "REML")
+# )
+
+
+
+get_betahat_sensitivity = function(betaTilde, L,
+                                   nknots_spec = "current",
+                                   splines = "tp",
+                                   method = "GCV.Cp") {
+  argvals = 1:L
+
+  # Knot specifications
+  nknots_options <- list(
+    "conservative" = min(round(L/4), 20),
+    "current" = min(round(L/2), 35),
+    "flexible" = min(round(3*L/4), 50),
+    "very_flexible" = min(L - 5, 70)
+  )
+
+  nknots <- nknots_options[[nknots_spec]]
+
+  betaHat <- t(apply(betaTilde, 1, function(x) {
+    fit <- mgcv::gam(x ~ s(argvals, bs = splines, k = (nknots + 1)),
+                     method = method)
+    fit$fitted.values
+  }))
+
+  rownames(betaHat) <- rownames(betaTilde)
+  colnames(betaHat) <- 1:L
+  return(betaHat)
+}
+
+get_cis_sensitivity = function(betaTilde_boot,
+                   betaHat,
+                   smooth_for_ci = TRUE,
+                   smooth_for_variance = TRUE,
+                   L,
+                   nknots_spec = "current",
+                   splines = "tp",
+                   method = "GCV.Cp",
+                   # nknots_min = 35,
+                   nknots_min_cov = 35,
+                   nknots_fpca = 35,
+                   mult_fac = 1.2) {
+  argvals = 1:L
+  B = ncol(betaTilde_boot[1, , ])
+  # nknots <- min(round(L / 2), nknots_min)
+  nknots_options <- list(
+    "conservative" = min(round(L/4), 20),
+    "current" = min(round(L/2), 35),
+    "flexible" = min(round(3*L/4), 50),
+    "very_flexible" = min(L - 5, 70)
+  )
+
+  nknots <- nknots_options[[nknots_spec]]
+
+  nknots_fpca <- min(round(L / 2), nknots_fpca)
+  betaHat_boot <- array(NA, dim = c(nrow(betaHat), ncol(betaHat), ncol(betaTilde_boot[1, , ])))
+  betaHat.var <- array(NA, dim = c(L, L, nrow(betaHat)))
+  # smooth
+
+
+  # for (b in 1:B) {
+  #   betaHat_boot[, , b] <- t(apply(betaTilde_boot[, , b], 1, function(x)
+  #     gam(x ~ s(
+  #       argvals, bs = "tp", k = (nknots + 1)
+  #     ), method = "GCV.Cp")$fitted.values))
+  # }
+
+  for (b in 1:B) {
+    betaHat_boot[, , b] <- t(apply(betaTilde_boot[, , b], 1, function(x)
+      bam(x ~ s(
+        argvals, bs = splines, k = (nknots + 1)
+      ), method = method)$fitted.values))
+  }
+
+  for (r in 1:nrow(betaHat)) {
+    if (smooth_for_variance) {
+      betaHat.var[, , r] <- mult_fac * var(t(betaHat_boot[r, , ]))
+    } else{
+      betaHat.var[, , r] <- mult_fac * var(t(betaTilde_boot[r, , ]))
+    }
+  }
+
+  N <- 10000
+  qn <- rep(0, length = nrow(betaHat))
+  set.seed(456)
+  for (i in 1:length(qn)) {
+    if (smooth_for_ci) {
+      est_bs <- t(betaHat_boot[i, , ])
+    } else{
+      est_bs <- t(betaTilde_boot[i, , ])
+    }
+    fit_fpca <- suppressWarnings(refund::fpca.face(est_bs, knots = nknots_fpca)) # suppress sqrt(Eigen$values) NaNs
+    ## extract estimated eigenfunctions/eigenvalues
+    phi <- fit_fpca$efunctions
+    lambda <- fit_fpca$evalues
+    K <- length(fit_fpca$evalues)
+
+    ## simulate random coefficients
+    theta <- matrix(stats::rnorm(N * K), nrow = N, ncol = K) # generate independent standard normals
+    if (K == 1) {
+      theta <- theta * sqrt(lambda) # scale to have appropriate variance
+      X_new <- tcrossprod(theta, phi) # simulate new functions
+    } else{
+      theta <- theta %*% diag(sqrt(lambda)) # scale to have appropriate variance
+      X_new <- tcrossprod(theta, phi) # simulate new functions
+    }
+    x_sample <- X_new + t(fit_fpca$mu %o% rep(1, N)) # add back in the mean function
+    Sigma_sd <- Rfast::colVars(x_sample, std = TRUE, na.rm = FALSE) # standard deviation: apply(x_sample, 2, sd)
+    x_mean <- colMeans(est_bs)
+    # x_sample: N x p matrix
+    # x_mean: length-p vector
+    # Sigma_sd: length-p vector
+
+    # Vectorized computation
+    z <- sweep(x_sample, 2, x_mean, "-")     # center
+    z <- sweep(z, 2, Sigma_sd, "/")          # standardize
+    un <- apply(abs(z), 1, max)              # row-wise max of absolute values
+
+    qn[i] <- stats::quantile(un, 0.95)
+  }
+
+  return(list(
+    betaHat = betaHat,
+    betaHat.var = betaHat.var,
+    qn = qn
+  ))
+
+}
---
+++ R/03b_run_survey_sim_smoothsens.R
@@ -0,0 +1,291 @@
+## smoothing parameter sensitivity analysis
+
+library(future)
+library(furrr)
+library(purrr)
+library(here)
+library(devtools)
+library(fastFMM)
+library(haven)
+library(survey)
+library(progress)
+library(lme4)
+library(mgcv)
+library(tidyfun)
+library(mvtnorm)
+library(refund)
+library(svrep)
+
+source(here::here("R", "create_survey_settings.R"))
+source(here::here("R", "01_sim_functions.R"))
+source(here::here("R", "01_sim_functions_smoothsens.R"))
+source(here::here("R", "00_data_gen_function_ff.R"))
+source(here::here("R", "00_data_gen_function_fast.R"))
+source(here::here("R", "utils.R"))
+
+
+force = FALSE
+force_iter = FALSE
+B = 500
+# ncores = parallelly::availableCores()
+ncores = future::availableCores()
+
+fit_types = c('weighted')
+boot_types = c('BRR')
+options(survey.lonely.psu = "adjust")
+
+
+ifold = get_fold()
+
+temp = smaller_grid[ifold,]
+
+nsim = switch(as.character(temp$len),
+              "50" = 200,
+              "100" = 200,
+              "1440" = 100)
+
+outfile = here::here("results", "simulations", "survey_sim_smoothsens", paste0("fold_", sprintf("%03d", ifold), ".rds"))
+if (file.exists(outfile)) {
+  x = read_rds(outfile)
+  if(nrow(x) >= nsim * length(boot_types) * 2) {
+    message("Output file already has results for all iterations. Exiting.")
+    quit(save = "no", status = 0)
+  } else if (force) {
+    message("Output file exists but force = TRUE, so re-running all iterations.")
+  } else {
+    message("Output file exists but does not have results for all iterations. Continuing to run missing iterations.")
+  }}
+
+if (!dir.exists(dirname(outfile))) dir.create(dirname(outfile), recursive = TRUE)
+partial_dir = here::here("results", "simulations", "survey_sim_smoothsens", paste0("fold_", sprintf("%03d", ifold), "_partials"))
+if (!dir.exists(partial_dir)) dir.create(partial_dir)
+partial_files = list.files(partial_dir, pattern = "^iter_\\d+\\.rds$", full.names = TRUE)
+
+if(length(partial_files) >= nsim) {
+  sim_res = map(partial_files, \(x) read_rds(x) %>% mutate(id = sub(".*iter_(.+)\\.rds.*", "\\1", basename(x)))) %>%
+    keep(., is.data.frame) %>%
+    list_rbind()
+
+  write_rds(sim_res, file = outfile)
+
+  unlink(partial_dir, recursive = TRUE)
+  message("All iterations complete, writing and exiting.")
+  quit(save = "no", status = 0)
+}
+
+if(temp$len == 1440) {
+  options(future.globals.maxSize = 100 * 1024^3)  # Equivalent to 1.13 GiB
+
+  structure = generate_population_structure(
+    I = 10e6,
+    L = temp$len,
+    family = temp$family,
+    seed = 4574,
+    num_strata = 30,
+    strata_sigma = temp$strata_sigma,
+    psu_factor = 0.5,
+    strata_scale = temp$strata_scale,
+    snr_b = temp$snr_b,
+    snr_eps = temp$snr_eps
+  )
+  structure$global_sd_lp = compute_global_sd_lp_fast(structure)
+  structure$snr_eps = temp$snr_eps  # match input
+
+  for (iter in seq_len(nsim)) {
+    if(file.exists(outfile)) {
+      x = read_rds(outfile)
+      c1 = unique(x$id) %>% as.integer()
+      c2 = list.files(partial_dir, pattern = "^iter_\\d+\\.rds$") %>%
+        str_extract("\\d+") %>%
+        as.integer()
+      completed_iters = c(c1, c2) %>% unique()
+    } else {
+      completed_iters = list.files(partial_dir, pattern = "^iter_\\d+\\.rds$") %>%
+        str_extract("\\d+") %>%
+        as.integer()
+    }
+    if (iter %in% completed_iters && !force_iter) {
+      message("Skipping completed iteration: ", iter)
+      next
+    }
+
+    set.seed(iter)
+    x = try({
+      data = sample_by_strata_stream_fast(
+        structure,
+        I_n = temp$In,
+        num_selected_psu = 2,
+        seed = iter,
+        inf_level = temp$inf_level,
+        compression = 2,
+        family = temp$family
+      )
+
+
+      beta_true = structure$beta_fixed
+
+       betaTilde = get_betatilde(data = data,
+                                family = temp$family,
+                                type = "weighted")
+
+      betaHat = get_betahat_sensitivity(betaTilde,
+                                        L = temp$len,
+                                        nknots_spec = temp$nknots_spec,
+                                        splines = temp$splines,
+                                        method = temp$method)
+
+      res = run_boots_xfast(data = data,
+                            boot_type = "BRR",
+                            betaHat = betaHat,
+                            family = temp$family,
+                            num_boots = B,
+                            L = temp$len,
+                            samp_stages = c("PPSWOR", "Poisson"))
+
+      cis = get_cis_sensitivity(betaTilde_boot = res,
+                                betaHat = betaHat,
+                                L = temp$len,
+                                smooth_for_ci = TRUE,
+                                nknots_spec = temp$nknots_spec,
+                                splines = temp$splines,
+                                method = temp$method)
+
+      stats = get_coverage_stats(mod_output = cis,
+                                 beta_true = bh_data,
+                                 name = iter,
+                                 L = temp$len) |>
+        mutate(n = nrow(data), n_boot = B)
+
+
+      write_rds(stats, file = file.path(partial_dir, sprintf("iter_%03d.rds", iter)))
+
+    })
+    rm(x)
+    print(iter)
+  }
+} else {
+
+  lst = generate_superpopulation(
+    family = temp$family,
+    I = 10e4,
+    L = temp$len,
+    snr_b = temp$snr_b,
+    snr_eps = temp$snr_eps,
+    strata_sigma = temp$strata_sigma,
+    strata_scale = temp$strata_scale,
+    seed = 111
+  )
+
+  if(file.exists(here::here(partial_dir, "bh_data.rds"))) {
+    bh_data = read_rds(here::here(partial_dir, "bh_data.rds"))
+  } else {
+    df = data.frame(X = lst$X_des[,2],
+                    Y = lst$Y_obs)
+
+    bt_data = get_betatilde(family = temp$family,
+                            type = "unweighted",
+                            data = df)
+    rm(df); gc()
+    bh_data  = get_betahat(bt_data, L = temp$len)
+
+    write_rds(bh_data, here::here(partial_dir, "bh_data.rds"))
+  }
+
+
+  for (iter in seq_len(nsim)) {
+
+    if(file.exists(outfile)) {
+      x = read_rds(outfile)
+      c1 = unique(x$id) %>% as.integer()
+      c2 = list.files(partial_dir, pattern = "^iter_\\d+\\.rds$") %>%
+        str_extract("\\d+") %>%
+        as.integer()
+      completed_iters = c(c1, c2) %>% unique()
+    } else {
+      completed_iters = list.files(partial_dir, pattern = "^iter_\\d+\\.rds$") %>%
+        str_extract("\\d+") %>%
+        as.integer()
+    }
+
+
+    if (iter %in% completed_iters && !force_iter) {
+      message("Skipping completed iteration: ", iter)
+      next
+    }
+
+    set.seed(iter)
+    x = try({
+      data = sample_from_population_wor(
+        X_des = lst$X_des,
+        Y_obs = lst$Y_obs,
+        L = temp$len,
+        I_n = temp$In,
+        num_strata = 30,
+        stratum_assignments = lst$stratum_assignments,
+        psu_assignments = lst$psu_assignments,
+        dirichlet_probs = lst$dirichlet_probs,
+        seed = iter,
+        inf_level = temp$inf_level,
+        compression = 2,
+        family = temp$family
+      )
+      print(summary(data$weight))
+
+      betaTilde = get_betatilde(data = data,
+                                family = temp$family,
+                                type = "weighted")
+
+      betaHat = get_betahat_sensitivity(betaTilde,
+                                        L = temp$len,
+                                        nknots_spec = temp$nknots_spec,
+                                        splines = temp$splines,
+                                        method = temp$method)
+
+      res = run_boots_xfast(data = data,
+                            boot_type = "BRR",
+                            betaHat = betaHat,
+                            family = temp$family,
+                            num_boots = B,
+                            L = temp$len,
+                            samp_stages = c("PPSWOR", "Poisson"))
+
+    cis = get_cis_sensitivity(betaTilde_boot = res,
+                              betaHat = betaHat,
+                              L = temp$len,
+                              smooth_for_ci = TRUE,
+                              nknots_spec = temp$nknots_spec,
+                              splines = temp$splines,
+                              method = temp$method)
+
+      stats = get_coverage_stats(mod_output = cis,
+                                 beta_true = bh_data,
+                                 name = iter,
+                                 L = temp$len) |>
+        mutate(n = nrow(data), n_boot = B)
+
+
+
+      write_rds(stats, file = file.path(partial_dir, sprintf("iter_%03d.rds", iter)))
+
+    })
+    rm(x)
+    print(iter)
+  }
+}
+
+
+partial_files = list.files(partial_dir, pattern = "^iter_\\d+\\.rds$", full.names = TRUE)
+
+sim_res = map(partial_files, \(x) read_rds(x) %>% mutate(id = sub(".*iter_(.+)\\.rds.*", "\\1", basename(x)))) %>%
+  keep(., is.data.frame) %>%
+  list_rbind()
+
+if (file.exists(outfile)) {
+  x = read_rds(outfile)
+  sim_res = sim_res %>% bind_rows(x)
+}
+write_rds(sim_res, file = outfile)
+
+
+unlink(partial_dir, recursive = TRUE)
+
